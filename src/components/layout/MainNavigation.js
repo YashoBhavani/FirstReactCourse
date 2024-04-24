5a2d2a7476cfc3add26fbd3cac170fac622efc63
Yashu <bhavani17498@gmail.com>
2024-04-24 15:28:05 -0500

    Added Styling for Navigation page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+
 function MainNavigation(){
     return (
-    <header>
-        <div>React Learning Project</div>
+    <header className={classes.header}>
+        <div className={classes.logo}>React Learning Project</div>
         <nav>
             <ul>
                 <li>
